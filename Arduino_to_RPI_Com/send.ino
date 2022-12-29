f6be5b76d360b008160416c8c95317d292773413
https:/// <pranavponns@gmail.com>
2022-12-28 23:10:50 -0500

    Added arduino comm code

---
+++ Arduino_to_RPI_Com/send.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <RF24.h>
+
+RF24 radio(9, 10);
+
+
+void setup(void){
+  Serial.begin(9600);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E1LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
+
+}
+
+void loop(void){
+    const char text[] = "Hello Monkey";
+    Serial.println(text[1]);
+    radio.write(&text, sizeof(text));
+    delay(1000);
+}
