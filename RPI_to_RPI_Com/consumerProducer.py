cbe21f15d6a79a6cf725bcf836ef2676c5ab3a8e
Pranav Ponnusamy <pranavponns@gmail.com>
2023-01-14 18:39:38 +0000

    resolved problems between conumser and analyzer thread

--- RPI_to_RPI_Com/consumerProducer.py
+++ RPI_to_RPI_Com/consumerProducer.py
@@ -1,103 +1,115 @@
 import queue
 import time
-import random
 from threading import Thread, Event
-
-from collections import deque
 
 import Adafruit_ADS1x15
 import neurokit2 as nk
+
+import copy
+
 
 import psutil
 
 
 GAIN = 2/3
-COLLECTION_TIME = 10
-SAMPLE_RATE = 1000
+COLLECTION_TIME = 60
+SAMPLE_RATE = 860
+ADC_CONVERSION_RATE = 1/SAMPLE_RATE
 
 
 # Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
 
-q = queue.Queue()  # ----- new data structure ----
+ppgSensorDataQueue = queue.Queue()  # ----- new data structure ----
 
-readings = []
+AnalyzerDataBuff = []
 
-process = Event()
-process.clear()
+isDataReady = Event()
+isDataReady.clear()
 
-class AnalyzerThread(Thread):
+class ppgAlgoThread(Thread):
     def run(self):
-        global readings
+        global AnalyzerDataBuff
         while True:
-            process.wait()
-            ppg_signal = readings
+            isDataReady.wait()
+            algoPpgSensorBuff = copy.deepcopy(AnalyzerDataBuff)
+            isDataReady.clear()
+            
+            # print(algoPpgSensorBuff)
+            print(len(algoPpgSensorBuff))
+            
             print('RAM memory % used:', psutil.virtual_memory()[2])
-            # start = time.time()
-            # arr = nk.ppg_simulate(duration=180, sampling_rate=1000, heart_rate=70)
-            # ppg_signal = list(arr)
-            ppg_cleaned = nk.ppg_clean(ppg_signal)
-            ppg_signals, info = nk.ppg_process(ppg_cleaned, sampling_rate = SAMPLE_RATE)
+            start = time.time()
+            
+            #arr = nk.ppg_simulate(duration=180, sampling_rate=1000, heart_rate=70)
+            # algoPpgSensorBuff = list(arr)
+            
+            # ppg_cleaned = nk.ppg_clean(algoPpgSensorBuff)
+            ppg_signals, info = nk.ppg_process(algoPpgSensorBuff, sampling_rate = SAMPLE_RATE)
             f_hrv = nk.hrv(ppg_signals)
             t_hrv = nk.hrv_time(ppg_signals)
             print("frequency domain hrv", f_hrv)
             print("time domain hrv", t_hrv)
             print("elaspsed time:", time.time()-start)
-            # ppg_signal.clear()
-            process.clear()
+            
 
-class ProducerThread(Thread):
+class SenReaderThread(Thread):
     def run(self):
         while True:
-            num = adc.read_adc(0, gain=GAIN)
-            q.put(num)      # ---- automatically synchronized 
-            time.sleep(0.0011627906977)
+            # Read current ADC conversion
+            num = adc.read_adc(0, gain=GAIN) 
+            ppgSensorDataQueue.put(num)      # ---- automatically synchronized 
+            time.sleep(ADC_CONVERSION_RATE)
 
-class ConsumerThread(Thread):
+class SenDataCollectorThread(Thread):
     def run(self):
-        l1 = []
-        l2 = []
-        l3 = []
-
-        buckets = [l1,l2,l3]
-        for bucket in buckets:
+        global AnalyzerDataBuff
+        senDataBuff1 = []
+        senDataBuff2 = []
+        senDataBuff3 = []
+        
+        # Each element in the list store a minute of sensor data 
+        ThreeMinSenDataList = [senDataBuff1, senDataBuff2, senDataBuff3]
+        
+        
+        #Fills the first three minutes sensor data
+        for OneMinSenData in ThreeMinSenDataList:
             start = time.time()
             while (time.time() - start <= COLLECTION_TIME):
-                num = q.get(True) # block if work queue empty ------
-                bucket.append(num)
-                # print(bucket , q.qsize())
+                lastSendData = ppgSensorDataQueue.get(True) # block if work queue empty ------
+                OneMinSenData.append(lastSendData)
+                # print(OneMinSenData , ppgSensorDataQueue.qsize())
 
         # combine and send list
-        readings = l1 + l2 + l3
-        # print(len(readings))
-        print(q.qsize())
-        #add flag to consume readings
-        process.set()
-        readings.clear()
+        AnalyzerDataBuff = ThreeMinSenDataList[0] + ThreeMinSenDataList[1] + ThreeMinSenDataList[2]
+        
+        # print(len(AnalyzerDataBuff))
+        print(ppgSensorDataQueue.qsize())
+        #add flag to consume AnalyzerDataBuff
+        isDataReady.set()
 
         while True:
-
             for x in range(0,3):
                 print("Filling bucket", x)
-                buckets[x].clear()
+                ThreeMinSenDataList[x].clear()
                 start = time.time()
                 while (time.time() - start <= COLLECTION_TIME):
-                    num = q.get(True) # block if work queue empty ------
-                    buckets[x].append(num)
-                    # print(buckets[x], q.qsize())
+                    num = ppgSensorDataQueue.get(True) # block if work queue empty ------
+                    ThreeMinSenDataList[x].append(num)
+                    # print(ThreeMinSenDataList[x], ppgSensorDataQueue.qsize())
                 #combine and send list 
                 
                 if x == 0:
-                    readings = l2 + l3 + l1
+                    AnalyzerDataBuff = senDataBuff2 + senDataBuff3 + senDataBuff1
                 elif x == 1:
-                    readings = l3 + l1 + l2
+                    AnalyzerDataBuff = senDataBuff3 + senDataBuff1 + senDataBuff2
                 else:
-                    readings = l1 + l2 + l3
-                # print(len(readings))
+                    AnalyzerDataBuff = senDataBuff1 + senDataBuff2 + senDataBuff3
+                    
+                # print(len(AnalyzerDataBuff))
                 print(time.time()-start)
-                process.set()
-                readings.clear()
+                isDataReady.set()
 
-producer = ProducerThread().start()
-consumer = ConsumerThread().start()
-analyzer = AnalyzerThread().start()
+producer = SenReaderThread().start()
+consumer = SenDataCollectorThread().start()
+analyzer = ppgAlgoThread().start()
